d7ac01a48bc36475b458b1f0fab94ef57ab31ef0
Angeliki Fokou <a@fokou.gr>
2024-12-28 23:29:22 +0200

    Exercise 6.9

--- anecdotes/src/components/AnecdoteList.jsx
+++ anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { voteAnecdote } from '../reducers/anecdoteReducer.js';
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => {
+    if (state.filter === '') {
+      return state.anecdotes
+    }
+    return state.anecdotes.filter(anecdote => anecdote.content.includes(state.filter))
+  })
   const dispatch = useDispatch()
 
   const vote = (id) => {
