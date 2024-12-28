65fb2275557333f990a8624d5896695c798fdbc3
Angeliki Fokou <a@fokou.gr>
2024-12-29 00:31:38 +0200

    Add notificationReducer

---
+++ anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState: '',
+  reducers: {
+    notificationChange: (state, action) => action.payload,
+  },
+})
+
+export const { notificationChange } = notificationSlice.actions
+export default notificationSlice.reducer
